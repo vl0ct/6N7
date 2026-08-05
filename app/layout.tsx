5b5e4b6ce4b4fc290d7491027ade8e3cd029702b
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-08-05 23:51:43 +0530

    feat: clerk org

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import { Inter, Outfit } from "next/font/google"
 
-import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs"
+import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs"
 import { shadcn } from "@clerk/ui/themes"
 
 import "./globals.css"
@@ -24,16 +30,17 @@ export default function RootLayout({
       className={cn("antialiased", inter.variable, outfitHeading.variable)}
     >
       <body>
-        <ClerkProvider appearance={{ theme: shadcn }}>
+        <ClerkProvider
+          appearance={{ theme: shadcn }}
+          taskUrls={{ "choose-organization": "/choose-organization" }}
+        >
           <ThemeProvider>
             <header>
               <Show when="signed-out">
                 <SignInButton />
                 <SignUpButton />
               </Show>
-              <Show when="signed-in">
-                {/* <UserButton /> */}
-              </Show>
+              <Show when="signed-in">{/* <UserButton /> */}</Show>
             </header>
             {children}
             <Toaster />
